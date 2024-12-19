b2d412a443f00bb281a3ef8fc86fc61e587a781b
Maojin-juan <edwardelric199887@gmail.com>
2024-12-19 22:39:44 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+<h1 align="center">Welcome to reactRouter 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-0.0.0-blue.svg?cacheSeconds=2592000" />
+</p>
+
+> reactRouter
+
+### 🏠 [Github](https://github.com/Maojin-juan/)
+
+## Install
+
+```sh
+npm install
+```
+
+## Usage
+
+```sh
+npm start
+```
+
+## Author
+
+👤 **毛巾**
+
+---
+
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+export default function Navbar() {
+  return (
+    <nav className="border-gray-200 bg-white dark:border-gray-700 dark:bg-gray-900">
+      <div className="mx-auto flex max-w-screen-xl flex-wrap items-center gap-4 p-4">
+        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="h-8"
+            alt="Flowbite Logo"
+          />
+          <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
+            Flowbite
+          </span>
+        </a>
+        <div
+          className="hidden w-full md:block md:w-auto"
+          id="navbar-multi-level"
+        >
+          <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:dark:bg-gray-900">
+            <li>
+              <a
+                href="#"
+                className="block rounded bg-blue-700 px-3 py-2 text-white md:bg-transparent md:p-0 md:text-blue-700 dark:bg-blue-600 md:dark:bg-transparent md:dark:text-blue-500"
+                aria-current="page"
+              >
+                Home
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
